7d183fc94fc29519dc4e23f9f06fa5b5098acdb8
YukiMinamiHHNii <homu.homu.nii@gmail.com>
2018-12-26 17:54:16 -0600

    Adds Docker-related files

--- src/controllers/timestampController.js
+++ src/controllers/timestampController.js
@@ -5,9 +5,6 @@ exports.getTime = function(req, res) {
 	});
 };
 exports.getTimestamp = function(req, res) {
-	console.log(req.params.date_string)
-	console.log(new Date(req.params.date_string))
-	console.log(isNaN(new Date(req.params.date_string)))
 	if (!isNaN(new Date(req.params.date_string).getTime())) {
 		res.json({
 			unix: parseInt(new Date(req.params.date_string).getTime() / 1000).toFixed(0),
